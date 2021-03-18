bc9b5fa61b7dc02c63f6a92701c74377619e2f66
ganiushina <32090008+ganiushina@users.noreply.github.com>
2021-03-18 12:54:34 +0400

    return user

--- src/main/java/ru/alta/rest/PersonRestController.java
+++ src/main/java/ru/alta/rest/PersonRestController.java
@@ -1,18 +1,36 @@
 package ru.alta.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.alta.security.model.User;
+import ru.alta.security.repository.UserRepository;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
 public class PersonRestController {
 
+   @Autowired
+   UserRepository userR;
+
    @GetMapping("/person")
-   public ResponseEntity<Person> getPerson() {
-      return ResponseEntity.ok(new Person("John Doe", "john.doe@test.org"));
-   }
+   public ResponseEntity<Optional<User>> getPerson() {
+      Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
+      String userName = loggedInUser.getName();
+      Optional<User> user = userR.findOneWithAuthoritiesByUsername (userName);
+//      String firstname = user.getFirstName();
+//      String email = user.getEmail();
+
+      return ResponseEntity.ok(user);
+
+//      return ResponseEntity.ok(new Person("John Doe", "john.doe@test.org"));
+}
 
    private static class Person {
 
